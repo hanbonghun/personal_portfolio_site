a6dab1980f8fe7087aed8a77e3c7fd72be562825
hanbonghun <bng4535@khu.ac.kr>
2023-03-20 04:58:40 +0900

    글쓰기 기능 구현, 화면 출력

--- post/model/Post.js
+++ post/model/Post.js
@@ -3,10 +3,11 @@ const postSchema = new mongoose.Schema({
   title: String,
   description: String,
   contents: String,
-  create_at: {
+  author : String,
+  createAt: {
     type: Date,
     default: new Date(),
-  },
+  }
 });
 
 module.exports = mongoose.model("Post", postSchema); 
